0ffc9fbf6a487afeb5452d260f3af4989a02bfca
Ibai Carretero <icarreterobdaw1@gmail.com>
2020-10-06 13:21:05 +0200

    nueva linea en test de  estudiante

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -20,6 +20,8 @@ public class TestEstudiante
         e3.printEstudiante();
         Estudiante e4 = new Estudiante("Roberto", 21);
         e4.printEstudiante();
+        Estudiante e5 = new Estudiante("Ibai", 17);
+        e5.printEstudiante();
 
     }
 }
